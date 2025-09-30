409d111c7fe3e8b9f902a771047ea5fba79b1b68
mtangaro <ma.tangaro@gmail.com>
2025-09-30 17:00:07 +0000

    reworked main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,52 +1,68 @@
 terraform {
+  required_version = ">= 1.4.0"
   required_providers {
-	cloudprovider = {
-  	source  = "<cloud-provider-specific>/provider"
-  	version = "~> X.Y.Z"
+	openstack = {
+  	source  = "terraform-provider-openstack/openstack"
+  	version = "~> 1.53.0"
 	}
   }
 }
  
-provider "cloudprovider" {
-  # authentication details here
-}
- 
-# SSH Key definition
-resource "cloudprovider_ssh_key" "vm_key" {
-  name   	= var.public_key["name"]
-  public_key = var.public_key["public_key"]
-}
- 
-# Private network definition
-resource "cloudprovider_network" "private_network" {
-  name = "private_network"
-}
- 
-# Security group allowing internal SSH from Bastion only
-resource "cloudprovider_security_group" "ssh_internal" {
-  name    	= "ssh-internal"
-  description = "Allow SSH from Bastion only"
- 
-  ingress {
-	from_port   = 22
-	to_port 	= 22
-	protocol	= "tcp"
-	cidr_blocks = ["<BASTION_PRIVATE_IP>/32"]
+provider "openstack" {
+  //cloud = "recas"
+  tenant_id = "d08a559abb2047f38ac447a332715d08"
+  auth_url = "https://keystone.recas.ba.infn.it/v3"
+  endpoint_overrides = {
+    "network"  = "https://neutron.recas.ba.infn.it/v2.0/"
+    "volumev3" = "https://cinder.recas.ba.infn.it/v3/"
+    "image" = "https://glance.recas.ba.infn.it/v2/"
   }
-}
- 
-# Galaxy VM configuration
-resource "cloudprovider_compute_instance" "galaxy_vm" {
-  name        	= "galaxy-private"
-  image_name  	= "RockyLinux 9.3"
-  flavor_name 	= "medium"
-  ssh_key_name	= cloudprovider_ssh_key.vm_key.name
-  security_groups = [cloudprovider_security_group.ssh_internal.id]
- 
-  network {
-	name = cloudprovider_network.private_network.name
-  }
- 
-  user_data = file("${path.module}/cloudinit.sh")
 }
 
+# SSH key definition
+resource "openstack_compute_keypair_v2" "vm_key" {
+  name       = "my-keypair"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZ6M41F4sf9ipRutlCNxGxpSNT9snjwIhaWbNiGKIr9 ma.tangaro@gmail.com"
+}
+
+# Private network definition
+data "openstack_networking_network_v2" "private_net" {
+  name = "private_net"
+}
+
+data "openstack_networking_subnet_v2" "private_subnet" {
+  name = "private_subnet"
+}
+
+# Security group allowing internal SSH from Bastion only
+resource "openstack_networking_secgroup_v2" "ssh_internal" {
+  name    	= "ssh-internal"
+  description = "Allow SSH from Bastion only"
+}
+
+# Rule: Allow SSH only from Bastion private IP
+resource "openstack_networking_secgroup_rule_v2" "ssh_from_bastion" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "212.189.202.200/32"
+  security_group_id = openstack_networking_secgroup_v2.ssh_internal.id
+}
+
+# Galaxy VM configuration
+resource "openstack_compute_instance_v2" "galaxy_vm" {
+  name        	= "galaxy-private"
+  image_name  	= "RockyLinux_9.5_20241118"
+  flavor_name 	= "xlarge"
+  key_pair    = openstack_compute_keypair_v2.vm_key.name
+  security_groups = ["default", openstack_networking_secgroup_v2.ssh_internal.name ]
+
+  network {
+    uuid = data.openstack_networking_network_v2.private_net.id
+  }
+
+
+  user_data = file("${path.module}/cloudinit.sh")
+}
